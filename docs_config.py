977ae679c84e89f334b8927d0ee8a44ba554830c
albertferal <albertferal@gmail.com>
2024-01-24 17:16:43 +0100

    trying pdoc

--- docs_config.py
+++ docs_config.py
@@ -1,9 +1,14 @@
+# docs_config.py
+
 # Añade el directorio raíz de tu aplicación al PYTHONPATH
 import sys
 sys.path.insert(0, './app')
 
 # Importa el módulo principal de tu aplicación
-from app.src.app import app
+from app.src.app import app  # Asegúrate de reemplazar "YourMainModule" con el nombre correcto
+
+# Importa el módulo application
+import application  # Asegúrate de reemplazar con el nombre correcto
 
 # Indica a pdoc los módulos que debe documentar
-modules = [app]
+modules = [app, application]  # Asegúrate de reemplazar "YourMainModule" con el nombre correcto
