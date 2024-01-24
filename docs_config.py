3a2d2360a993b86e3d4cfe61c0a92e7610c8852b
albertferal <albertferal@gmail.com>
2024-01-24 15:50:36 +0100

    pdoc

--- docs_config.py
+++ docs_config.py
@@ -1,14 +1,15 @@
 # docs_config.py
 
-# Configuración para pdoc
-# Aquí debes incluir todas las configuraciones necesarias para que pdoc pueda importar y documentar tu código.
+import sys
+import os
 
 # Añade el directorio raíz de tu aplicación al PYTHONPATH
-import sys
-sys.path.insert(0, './app')
+current_dir = os.path.abspath(os.path.dirname(__file__))
+app_dir = os.path.join(current_dir, 'app')
+sys.path.insert(0, app_dir)
 
 # Importa el módulo principal de tu aplicación
-from app.src.app import app  # Asegúrate de reemplazar "YourMainModule" con el nombre correcto
+from app.src.app import app
 
 # Indica a pdoc los módulos que debe documentar
-modules = [app]  # Asegúrate de reemplazar "YourMainModule" con el nombre correcto
+modules = [app]
