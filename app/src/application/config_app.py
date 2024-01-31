85540fce588172c1f6029e7d25d535e219cd333d
RAFAEL TORICES <torices.rafael@gmail.com>
2024-01-31 08:29:09 +0100

    values default modified

--- app/src/application/config_app.py
+++ app/src/application/config_app.py
@@ -51,21 +51,21 @@ class ConfigApp():
     URL_WEBHOOK = os.getenv('URL_WEBHOOK')
     APIURL = os.getenv('API_TELEGRAM')
     TOKEN = os.getenv('TOKEN')
-    TITULO_APP = os.getenv('TITULO_APP', 'TRIANGLE-App') + '\n\n'
+    TITULO_APP = os.getenv('TITULO_APP', 'TITULO_APP') + '\n\n'
     TELEFONO_SOPORTE = os.getenv(
-        'TELEFONO_SOPORTE', '900000000') + '\n\n'
+        'TELEFONO_SOPORTE', '000000000') + '\n\n'
     CHAT_ID_SOPORTE = os.getenv(
         'CHAT_ID_SOPORTE', '000000') + '\n\n'
-    EMAIL_SOPORTE = os.getenv('EMAIL_SOPORTE', 'MyEMAIL') + '\n\n'
+    EMAIL_SOPORTE = os.getenv('EMAIL_SOPORTE', 'EMAIL_SOPORTE') + '\n\n'
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'usermysql')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'passwordmysql')
-    MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'databasemysql')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'mysql')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'mysql')
+    MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'mysql')
     MYSQL_PORT = os.getenv('MYSQL_PORT', 3306)
-    WEB_USERNAME = os.getenv('WEB_USERNAME', 'userweb')
-    WEB_PASSWORD = os.getenv('WEB_PASSWORD', 'passwordweb')
+    WEB_USERNAME = os.getenv('WEB_USERNAME', 'web')
+    WEB_PASSWORD = os.getenv('WEB_PASSWORD', 'web')
     REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
     MONGO_HOST = os.getenv('MONGO_HOST', 'localhost')
     MONGO_PORT = os.getenv('MONGO_PORT', 27017)
-    MONGO_DATABASE = os.getenv('MONGO_DATABASE', 'databasemongo')
-    MONGO_COLLECTION = os.getenv('MONGO_COLLECTION', 'collectionmongo')
+    MONGO_DATABASE = os.getenv('MONGO_DATABASE', 'mongo')
+    MONGO_COLLECTION = os.getenv('MONGO_COLLECTION', 'mongo')
