388c756ae126b1e5fa0bed8980a9550b5e7fefbd
KeepDevOpsTriangel <157544258+KeepDevOpsTriangel@users.noreply.github.com>
2024-01-31 01:00:28 +0100

    Update test_app.py para que pasen los tests

--- app/src/tests/test_app.py
+++ app/src/tests/test_app.py
@@ -7,24 +7,24 @@ from application.config_app import ConfigApp
 
 def test_default_titulo_app():
     config = ConfigApp()
-    assert config.TITULO_APP == 'Valor_Predeterminado\n\n'
+    assert config.TITULO_APP == 'TRIANGLE-App'
 
 ##################################################################################
 
 def test_default_telf_app():
     config = ConfigApp()
-    assert config.TELEFONO_SOPORTE == 'Valor_Predeterminado\n\n'
+    assert config.TELEFONO_SOPORTE == '900000000'
 
 ##################################################################################
     
 def test_default_chat_id_app():
     config = ConfigApp()
-    assert config.CHAT_ID_SOPORTE == 'Valor_Predeterminado\n\n'
+    assert config.CHAT_ID_SOPORTE == '000000'
 
 ##################################################################################
     
 def test_default_email_app():
     config = ConfigApp()
-    assert config.EMAIL_SOPORTE == 'Valor_Predeterminado\n\n'
+    assert config.EMAIL_SOPORTE == 'MyEMAIL'
 
 ##################################################################################
