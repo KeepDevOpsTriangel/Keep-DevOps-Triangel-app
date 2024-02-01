4f07ddda8d3933d514bfbd25c84a306638f1e6c7
RAFAEL TORICES <torices.rafael@gmail.com>
2024-02-01 19:12:10 +0100

    num nodes gke and port number https values helm

--- gke/root/main.tf
+++ gke/root/main.tf
@@ -3,7 +3,7 @@ module "gke" {
   resource_name        = var.resource_name
   project_id           = var.project_id
   cluster_location     = var.zone
-  cluster_num_nodes    = 1
+  cluster_num_nodes    = 2
   cluster_machine_type = "n1-standard-1"
   cluster_disk_type    = "pd-standard"
   cluster_disk_size_gb = 25
