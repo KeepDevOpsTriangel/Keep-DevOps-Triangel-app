cbf7bbf3fef1ab6631b7227dcd22b850340d4eed
RAFAEL TORICES <torices.rafael@gmail.com>
2024-02-07 13:52:20 +0100

    chatbot finally temp

--- app/src/application/resp.py
+++ app/src/application/resp.py
@@ -7,6 +7,9 @@ from application.config_app import ConfigApp  # Import class configApp
 from application.options import Options  # Import class Options
 from application.context import Context  # Import class Context
 from application.chatbot import ChatBot  # Import class ChatBot
+from application.mongodb import MongoDB  # Import class MongoDB
+import time  # Import time for use time
+import datetime  # Import datetime for use datetime
 
 
 class RespText():
@@ -26,6 +29,35 @@ class RespText():
         self.user = User()
         self.context = Context()
         self.chatbot = ChatBot()
+        self.mongodb = MongoDB()
+
+    def SaveMessage(self, text):
+        """
+        save message of chatbot in MongoDB
+
+        args:
+        ----------
+        self : object
+            Object of class
+        chatId : str
+            Id of chat user
+        text : str
+            Text of message
+        """
+        time_unix = int(time.time())
+        time_text = datetime.datetime.fromtimestamp(time_unix)
+        time_text = time_text.strftime('%d-%m-%Y %H:%M:%S')
+        message = {
+            "message": {
+                "date": time_text,
+                "text": text,
+                "chat": {
+                    "first_name": self.config.TITULO_APP,
+                    "username": "ChatBot",
+                }
+            }
+        }
+        self.mongodb.InsertMessage(message)
 
     def SendResponse(self, chatId, text, first_name):
         """
@@ -69,24 +101,22 @@ class RespText():
             result = self.config.TITULO_APP + options[0][1] + "\n"
             self.api.SendMessage(
                 chatId, result)
+            self.SaveMessage(result)
         elif text == options[1][0]:
             result = self.config.TITULO_APP + options[1][1] + "\n"
             self.api.SendMessage(
                 chatId, result)
+            self.SaveMessage(result)
         elif text == options[2][0]:
             result = self.config.TITULO_APP + options[2][1] + "\n"
             self.api.SendMessage(
                 chatId, result)
+            self.SaveMessage(result)
         elif text == options[3][0]:
             result = self.config.TITULO_APP + options[3][1] + "\n"
             self.api.SendMessage(
                 chatId, result)
-        elif text == 'hola' or text == 'Hola' or text == 'HOLA':
-            result = self.config.TITULO_APP + "Hola " + first_name + \
-                ", ¿En qué puedo ayudarte? \n\nTambién puedes seleccionar una de mis opciones\n"
-            self.api.SendMessage(chatId, result)
-            self.api.SendMessage(chatId, '/OPTIONS')
-            self.api.SendKeyboard(chatId, self.options.GetOptions())
+            self.SaveMessage(result)
         else:
             self.api.SendMessage(
                 chatId, self.config.TITULO_APP +
